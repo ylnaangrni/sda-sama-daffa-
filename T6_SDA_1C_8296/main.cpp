aa62024fdf60e7ca5bfe8603ad5ed1b06558ab25
Yulina Anggraeni <yulina.anggraeni.tif23@polban.ac.id>
2024-04-02 19:48:13 +0700

    lina

--- T6_SDA_1C_8296/main.cpp
+++ T6_SDA_1C_8296/main.cpp
@@ -8,16 +8,18 @@ int main()
 
     do
     {
-        std::cout << "Pilih operasi yang ingin dilakukan:\n";
-        std::cout << "1. Tambah kota\n";
-        std::cout << "2. Tambah anggota awal ke kota\n";
-        std::cout << "3. Tambah anggota tengah ke kota\n";
-        std::cout << "4. Hapus anggota dari kota\n";
-        std::cout << "5. Tampilkan daftar kota\n";
-        std::cout << "6. Tampilkan daftar anggota kota\n";
-        std::cout << "7. Cari data kota\n";
-        std::cout << "8. tambah penduduk akhir ke kota\n";
-        std::cout << "0. Keluar\n";
+        std::cout << "=======================================\n";
+        std::cout << "| Pilih operasi yang ingin dilakukan: |\n";
+        std::cout << "| 1. Tambah kota                      |\n";
+        std::cout << "| 2. Tambah anggota awal ke kota      |\n";
+        std::cout << "| 3. Tambah anggota tengah ke kota    |\n";
+        std::cout << "| 4. tambah penduduk akhir ke kota    |\n";
+        std::cout << "| 5. Hapus anggota dari kota          |\n";
+        std::cout << "| 6. Tampilkan daftar kota            |\n";
+        std::cout << "| 7. Tampilkan daftar anggota kota    |\n";
+        std::cout << "| 8. Cari data kota                   |\n";
+        std::cout << "| 0. Keluar                           |\n";
+        std::cout << "=======================================\n";
         std::cout << "Pilihan: ";
 
         std::cin >> opsi;
@@ -58,35 +60,36 @@ int main()
         case '4':
             std::cout << "Masukkan nama kota: ";
             std::cin >> namaKota;
-            std::cout << "Masukkan nama anggota yang ingin dihapus: ";
-            std::cin >> namaPenduduk;
-            hapusPenduduk(namaKota, namaPenduduk);
-            std::cout << "Anggota " << namaPenduduk << " berhasil dihapus dari kota " << namaKota << ".\n";
-            break;
-        case '5':
-            std::cout << "Daftar kota:\n";
-            tampilkanDaftarKota();
-            break;
-        case '6':
-            std::cout << "Masukkan nama kota: ";
-            std::cin >> namaKota;
-            std::cout << "Daftar anggota kota " << namaKota << ":\n";
-            tampilkanDaftarAnggota(namaKota);
-            break;
-        case '7':
-            std::cout << "Masukkan nama kota yang ingin dicari: ";
-            std::cin >> namaKota;
-            cariDataKota(namaKota);
-            break;
-        case '8':
-        std::cout << "Masukkan nama kota: ";
-            std::cin >> namaKota;
             std::cout << "Masukkan nama anggota: ";
             std::cin >> namaPenduduk;
             std::cout << "Masukkan usia anggota: ";
             std::cin >> usiaPenduduk;
             tambahPendudukAkhir(namaKota, namaPenduduk, usiaPenduduk);
             std::cout << "Anggota " << namaPenduduk << " (usia " << usiaPenduduk << ") berhasil ditambahkan ke kota " << namaKota << ".\n";
+    
+        case '5':
+            std::cout << "Masukkan nama kota: ";
+            std::cin >> namaKota;
+            std::cout << "Masukkan nama anggota yang ingin dihapus: ";
+            std::cin >> namaPenduduk;
+            hapusPenduduk(namaKota, namaPenduduk);
+            std::cout << "Anggota " << namaPenduduk << " berhasil dihapus dari kota " << namaKota << ".\n";
+            break;
+        case '6':
+            std::cout << "Daftar kota:\n";
+            tampilkanDaftarKota();
+            break;
+        case '7':
+            std::cout << "Masukkan nama kota: ";
+            std::cin >> namaKota;
+            std::cout << "Daftar anggota kota " << namaKota << ":\n";
+            tampilkanDaftarAnggota(namaKota);
+            break;
+        case '8':
+            std::cout << "Masukkan nama kota yang ingin dicari: ";
+            std::cin >> namaKota;
+            cariDataKota(namaKota);
+            break;
     
         default:
             std::cout << "Pilihan tidak valid. Silakan pilih lagi.\n";
